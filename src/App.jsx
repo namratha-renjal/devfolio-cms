ec3692d79a7742717c1c79ea76623a0e4b75ab31
namratha-renjal <namratha2311@gmail.com>
2025-11-04 19:58:39 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+  return (
+    <div>Hello World
+      <Button className="bg-blue-800">Click Me</Button>
+    </div>
+
+  )
+}
+
+export default App
